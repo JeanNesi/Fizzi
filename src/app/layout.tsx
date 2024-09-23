2f4e083dc53cb2514d81fed24f7724c6bf67b341
JeanNesi <jeannesi.carlos@gmail.com>
2024-09-23 10:18:37 -0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "@/prismicio";
+import localFont from "next/font/local";
+
+import "./globals.css";
+import Header from "@/components/Header";
+import ViewCanvas from "@/components/ViewCanvas";
+
+const alpino = localFont({
+  src: "../../public/fonts/Alpino-Variable.woff2",
+  display: "swap",
+  weight: "100 900",
+  variable: "--font-alpino",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={alpino.className}>
+      <body className="overflow-x-hidden bg-yellow-300">
+        <Header />
+        <main>{children}</main>
+
+        <ViewCanvas />
+      </body>
+      <PrismicPreview repositoryName={repositoryName} />
+    </html>
+  );
+}
